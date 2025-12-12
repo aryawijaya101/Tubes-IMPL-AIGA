b9be3568f57f7d99e98da3c205ae0b60d05b944e
fl111x <ammar.bfw@gmail.com>
2025-12-12 22:57:09 +0700

    update: class entity

---
+++ Aplikasi_Booking_Lapangan/src/model/Entity/User.java
@@ -0,0 +1,84 @@
+package model.Entity;
+
+public class User {
+    private int userId;
+    private String nama;
+    private String email;
+    private String password;
+    private String phone;
+    private String role;
+
+
+    public User() {
+    }
+
+
+    public User(int userId, String nama, String email, String password, String phone, String role) {
+        this.userId = userId;
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.role = role;
+    }
+
+
+    public User(String nama, String email, String password, String phone, String role) {
+        this.nama = nama;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.role = role;
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+
+}
