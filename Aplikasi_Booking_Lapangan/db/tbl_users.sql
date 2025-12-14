5e1e865c5396346764d7a600726e7edc918b3f88
fl111x <ammar.bfw@gmail.com>
2025-12-14 10:44:17 +0700

    update: fix db auto increment user_id

--- Aplikasi_Booking_Lapangan/db/tbl_users.sql
+++ Aplikasi_Booking_Lapangan/db/tbl_users.sql
@@ -2,7 +2,7 @@ USE field_booking_db;
 DROP TABLE IF EXISTS tbl_users;
 
 CREATE TABLE tbl_users(
-    user_id INT(16) NOT NULL PRIMARY KEY,
+    user_id INT(16) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nama_lengkap VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
